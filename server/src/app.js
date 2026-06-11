6ddf3c9249bcc38282cd9f67a532f688ee8bb546
Hariharan S <hariharansankar004@gmail.com>
2026-06-11 08:13:59 +0530

    Updated CORS settings

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,10 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: true,
+    origin: [
+        'https://vercel-deployment-gray-beta.vercel.app',
+        'http://localhost:5173'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
